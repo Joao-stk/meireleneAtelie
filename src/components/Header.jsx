c825ad917457d1621ca26fcd6f70270ae6f6dca0
João Felipe <joaoufelipe@hotmail.com>
2025-09-28 03:40:27 +0000

    Ajuste logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <header className="bg-gradient-to-r from-rose-50 to-rose-100 shadow-md p-4 flex items-center">
       <Link to={"/"}>
-      <img src={uteis.imgLogo} alt="logo" className="h-12" />
+      <img src={logo} alt="logo" className="h-12" />
       <h2 className="text-yellow-950">Meirelene Atêlie</h2> 
       </Link>
       <nav className="flex gap-3 right-0 ml-auto">
