bc30896a6bb8cbdeb98c8931cfc5d8927fdb61b9
João Felipe <joaoufelipe@hotmail.com>
2025-10-02 15:08:58 +0000

    Ajustando banner mobile

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,13 +4,17 @@ import banner from "../assets/banner.jpeg";
 export default function Banner() {
   return (
     <div
-      className="relative w-full max-w-screen overflow-hidden h-screen md:h-[80vh] min-h-[400px] bg-cover bg-center"
+      className="
+        relative w-full overflow-hidden 
+        h-[40vh] sm:h-[60vh] md:h-[80vh] lg:h-[90vh] 
+        bg-cover bg-center
+      "
       style={{
         backgroundImage: `url(${banner})`,
       }}
     >
       {/* Gradiente Overlay */}
-      <div className="absolute inset-0 bg-gradient-to-b from-[#654321]/40 via-[#545454]/30 to-transparent"></div>
+      <div className="absolute inset-0 bg-gradient-to-b from-[#000000]/40 via-transparent to-[#000000]/30"></div>
     </div>
   );
 }
